c3e0933a4ff3dd44b86b078ba34ec1907a599cfb
will grant <will@willgrant.org>
2021-01-21 13:14:26 +0000

    remove topsites

--- render.js
+++ render.js
@@ -23,24 +23,6 @@ chrome.storage.local.get(['data'], storeData => {
     document.getElementById('productCategory').innerHTML = storeData.data[index][3];
 });
 
-
-// Get the topsites from the browser and parse those in too
- 
-function buildPopupDom(mostVisitedURLs) {
-  var popupDiv = document.getElementById('mostVisited_div');
-  var ul = popupDiv.appendChild(document.createElement('ul'));
-
-  for (var i = 0; i < 7; i++) {
-    var li = ul.appendChild(document.createElement('li'));
-    var a = li.appendChild(document.createElement('a'));
-    a.href = mostVisitedURLs[i].url;
-    a.appendChild(document.createTextNode(mostVisitedURLs[i].title));
-    a.addEventListener('click', onAnchorClick);
-  }
-}
-chrome.topSites.get(buildPopupDom);
-
-
 // show DDG tip on search bar focus
 
 var el = document.getElementById('searchBox');
